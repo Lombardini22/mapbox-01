2b34222d2d2ba43b2ed81f78f5febd7e7fe1ac2e
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2022-02-23 11:49:55 +0100

    Updated popup and colors.

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
+const pins = [
+  {
+    coords: [11.612585832942786, 44.42633415796408],
+    color: "green",
+    imageUrl: "./cover-image.jpg",
+    url: "https://www.google.com",
+    title: "CSP",
+    address: "Via Lorem Ipsum, 42, 12345 Paese (PV), IT",
+  },
+  {
+    coords: [10.20748084609425, 45.557005241095766],
+    color: "green",
+    imageUrl: "./cover-image.jpg",
+    url: "https://www.google.com",
+    title: "Brescia",
+    address: "Via Lorem Ipsum, 42, 12345 Paese (PV), IT",
+  },
+  {
+    coords: [10.32337522358069, 45.458929165211245],
+    color: "white",
+    imageUrl: "./cover-image.jpg",
+    url: "https://www.google.com",
+    title: "Montichiari",
+    address: "Via Lorem Ipsum, 42, 12345 Paese (PV), IT",
+  },
+  {
+    coords: [10.42709859586792, 44.82709859586792],
+    color: "black",
+    imageUrl: "./cover-image.jpg",
+    url: "https://www.google.com",
+    title: "Parma",
+    address: "Via Lorem Ipsum, 42, 12345 Paese (PV), IT",
+  },
+  {
+    coords: [11.72709859586792, 44.82709859586792],
+    color: "black",
+    imageUrl: "./cover-image.jpg",
+    url: "https://www.google.com",
+    title: "Ferrara",
+    address: "Via Lorem Ipsum, 42, 12345 Paese (PV), IT",
+  },
+];
+
 window.addEventListener("DOMContentLoaded", () => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoiZnVkZmFjdG9yeSIsImEiOiJja3plM2VhcnowZTB0MnVvY25ucTFmZ2tlIn0.pAyQhvDwW-cWBmJHvUe-tQ";
-
-  const pins = [
-    createPin(
-      44.42633415796408,
-      11.612585832942786,
-      '<a href="https://www.google.com" target="_blank" rel="nofollow">CSP</a>',
-      "green"
-    ),
-    createPin(
-      45.557005241095766,
-      10.20748084609425,
-      '<a href="https://www.google.com" target="_blank" rel="nofollow">Brescia</a>',
-      "green"
-    ),
-    createPin(
-      45.458929165211245,
-      10.32337522358069,
-      '<a href="https://www.google.com" target="_blank" rel="nofollow">Montichiari</a>',
-      "red"
-    ),
-    createPin(
-      45.527078,
-      10.929078,
-      '<a href="https://www.google.com" target="_blank" rel="nofollow">Verona</a>',
-      "image"
-    ),
-  ];
 
   const map = new mapboxgl.Map({
     container: "map",
@@ -37,21 +53,22 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   pins.forEach((pin) => {
-    const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(pin.description);
-    const el = document.createElement("div");
+    const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+      <img
+        class="l22-custom-popup-content-image"
+        src="${pin.imageUrl}"
+        alt="${pin.title}"
+      >
+      <div class="l22-custom-popup-content">
+        <h4>${pin.title}</h4>
+        <p>${pin.address}</p>
+        <a href="${pin.url}" target="_blank" rel="noopen">Scopri di più</a>
+      </div>
+    `);
 
+    const el = document.createElement("div");
     el.classList.add("marker", pin.color);
 
     new mapboxgl.Marker(el).setLngLat(pin.coords).setPopup(popup).addTo(map);
   });
 });
-
-/**
- * @param {number} lat
- * @param {number} lon
- * @param {string} description
- * @param {string} color
- */
-function createPin(lat, lon, description, color) {
-  return { coords: [lon, lat], description, color };
-}
